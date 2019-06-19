d1424fd8f3685b076a364af16c38288c914f26ac
InKwon James Kim <devikkim@gmail.com>
2019-06-19 10:24:45 +0900

    Initial Commit

---
+++ KoreaJusoAPIDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  KoreaJusoAPIDemo
+//
+//  Created by InKwon Devik Kim on 19/06/2019.
+//  Copyright © 2019 InKwon Devik Kim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
